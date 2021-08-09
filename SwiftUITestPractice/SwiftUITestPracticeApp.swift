974784fc5cd03cd12fcf3ff707a305d0d582ca05
Andronick Martusheff <71955336+martusheff@users.noreply.github.com>
2021-08-09 14:17:15 -0700

    Initial Commit

---
+++ SwiftUITestPractice/SwiftUITestPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITestPracticeApp.swift
+//  SwiftUITestPractice
+//
+//  Created by andronick martusheff on 8/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITestPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
